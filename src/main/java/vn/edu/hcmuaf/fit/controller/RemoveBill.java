5b40ec9e570df15cd15e6123f99f00db8eee3698
KaAnThienPhuc <20130096@st.hcmuaf.edu.vn>
2023-04-14 14:21:11 +0700

    Mod can't delete in BillAdmin

--- src/main/java/vn/edu/hcmuaf/fit/controller/RemoveBill.java
+++ src/main/java/vn/edu/hcmuaf/fit/controller/RemoveBill.java
@@ -1,6 +1,8 @@
 package vn.edu.hcmuaf.fit.controller;
 
 import vn.edu.hcmuaf.fit.Dao.BillDao;
+import vn.edu.hcmuaf.fit.model.User;
+import vn.edu.hcmuaf.fit.services.PermissionService;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -9,9 +11,26 @@ import java.io.IOException;
 
 @WebServlet(name = "RemoveBill", value = "/RemoveBill")
 public class RemoveBill extends HttpServlet {
+    private static  String name = "bill";
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if(request.getSession().getAttribute("auth")==null){
+            response.sendRedirect("/errorAccessUser.jsp");
+            return;
+        }
+        int per = PermissionService.getInstance().checkAccess(name, ((User)(request.getSession().getAttribute("auth"))).getId());
+        if(per==2) {
+            response.sendRedirect("/errorAccessUser.jsp");
+            return;
+        }
+        if(per==1) {
+            response.sendRedirect("/AdminWeb/errorAccessAdmin.jsp");
+            return;
+        }
         String id = request.getParameter("id");
+
+
+
         BillDao.getInstance().delete(id);
     }
 
