4356ae71b4a1deee088cc937dcca0af149bd86c7
KaAnThienPhuc <20130096@st.hcmuaf.edu.vn>
2023-04-14 14:47:19 +0700

    Log in update type

--- src/main/java/vn/edu/hcmuaf/fit/controller/UpdateTypeAdmin.java
+++ src/main/java/vn/edu/hcmuaf/fit/controller/UpdateTypeAdmin.java
@@ -2,13 +2,17 @@ package vn.edu.hcmuaf.fit.controller;
 
 import vn.edu.hcmuaf.fit.Dao.ProductDao;
 import vn.edu.hcmuaf.fit.Dao.TypeProductDao;
+import vn.edu.hcmuaf.fit.bean.Log;
+import vn.edu.hcmuaf.fit.database.DB;
 import vn.edu.hcmuaf.fit.model.Product;
 import vn.edu.hcmuaf.fit.model.TypeProduct;
+import vn.edu.hcmuaf.fit.model.User;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.util.List;
 
 @WebServlet(name = "UpdateTypeAdmin", value = "/UpdateTypeAdmin")
@@ -31,9 +35,18 @@ public class UpdateTypeAdmin extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        InetAddress addr = InetAddress.getLocalHost();
+
+        //Host IP Address
+        String ipAddress = addr.getHostAddress();
+        //Hostname
+        String hostname = addr.getHostName();
+        User uu = (User) request.getSession().getAttribute("auth");
         String id = request.getParameter("id");
         String name = request.getParameter("name");
         String idType = request.getParameter("typeFather");
+
+        DB.me().insert(new Log(Log.WARNING,uu.getId(),ipAddress,"MANAGE TYPE PRODUCT","Sửa loại sản phẩm: "+name+", phân loại cha: " +TypeProductDao.getInstance().selectTypeName(idType),0));
 
         TypeProductDao.getInstance().update(id, name,idType);
         response.sendRedirect("/TypeAdmin");
