336193cb22da57aca14b1449757d201f69f382f7
KaAnThienPhuc <20130096@st.hcmuaf.edu.vn>
2023-04-14 14:50:44 +0700

    Log in remove type

--- src/main/java/vn/edu/hcmuaf/fit/controller/RemoveTypeAdmin.java
+++ src/main/java/vn/edu/hcmuaf/fit/controller/RemoveTypeAdmin.java
@@ -2,17 +2,30 @@ package vn.edu.hcmuaf.fit.controller;
 
 import vn.edu.hcmuaf.fit.Dao.ProductDao;
 import vn.edu.hcmuaf.fit.Dao.TypeProductDao;
+import vn.edu.hcmuaf.fit.bean.Log;
+import vn.edu.hcmuaf.fit.database.DB;
+import vn.edu.hcmuaf.fit.model.User;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.net.InetAddress;
 
 @WebServlet(name = "RemoveTypeAdmin", value = "/RemoveTypeAdmin")
 public class RemoveTypeAdmin extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String idP = request.getParameter("idP");
+        InetAddress addr = InetAddress.getLocalHost();
+
+        //Host IP Address
+        String ipAddress = addr.getHostAddress();
+        //Hostname
+        String hostname = addr.getHostName();
+        User uu = (User) request.getSession().getAttribute("auth");
+        DB.me().insert(new Log(Log.DANGER,uu.getId(),ipAddress,"MANAGE TYPE PRODUCT","Xóa loại sản phẩm: "+TypeProductDao.getInstance().selectTypeName(idP),0));       
+
         TypeProductDao.getInstance().delete(idP);
 
     }
