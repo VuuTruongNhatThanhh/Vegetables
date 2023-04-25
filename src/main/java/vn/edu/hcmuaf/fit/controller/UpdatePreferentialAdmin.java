47911fdebd18fe8526d410e6169e4401cdc6a70d
VuuTruongNhatThanhh <116423300+VuuTruongNhatThanhh@users.noreply.github.com>
2023-04-25 14:01:32 +0700

    Log in update discount code

--- src/main/java/vn/edu/hcmuaf/fit/controller/UpdatePreferentialAdmin.java
+++ src/main/java/vn/edu/hcmuaf/fit/controller/UpdatePreferentialAdmin.java
@@ -2,13 +2,17 @@ package vn.edu.hcmuaf.fit.controller;
 
 import vn.edu.hcmuaf.fit.Dao.PreferentialDao;
 import vn.edu.hcmuaf.fit.Dao.TypeProductDao;
+import vn.edu.hcmuaf.fit.bean.Log;
+import vn.edu.hcmuaf.fit.database.DB;
 import vn.edu.hcmuaf.fit.model.Preferential;
 import vn.edu.hcmuaf.fit.model.TypeProduct;
+import vn.edu.hcmuaf.fit.model.User;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.util.List;
 
 @WebServlet(name = "UpdatePreferentialAdmin", value = "/UpdatePreferentialAdmin")
@@ -27,11 +31,19 @@ public class UpdatePreferentialAdmin extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        InetAddress addr = InetAddress.getLocalHost();
+
+        //Host IP Address
+        String ipAddress = addr.getHostAddress();
+        //Hostname
+        String hostname = addr.getHostName();
+        User uu = (User) request.getSession().getAttribute("auth");
         String id = request.getParameter("id");
         String price = request.getParameter("price");
         String dayStart = request.getParameter("dayStart");
         String dayEnd = request.getParameter("dayEnd");
 
+        DB.me().insert(new Log(Log.WARNING,uu.getId(),ipAddress,"Quản lý ưu đãi","Sửa mã ưu đãi: "+id+", giá tiền được giảm: " +price+", ngày bắt đầu: "+ dayStart+", ngày kết thúc: "+ dayEnd,0));
        PreferentialDao.getInstance().update(id, price, dayStart, dayEnd);
         response.sendRedirect("/PreferentialAdmin");
     }
