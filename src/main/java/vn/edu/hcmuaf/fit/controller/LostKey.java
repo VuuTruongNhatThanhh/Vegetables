f0f73b24d380e3445a5626fe9e4647cb5752411f
Bùi Quốc Long <115862427+buiquoclong@users.noreply.github.com>
2024-01-10 09:58:09 +0700

    Update LostKey.java

--- src/main/java/vn/edu/hcmuaf/fit/controller/LostKey.java
+++ src/main/java/vn/edu/hcmuaf/fit/controller/LostKey.java
@@ -13,7 +13,16 @@ import java.security.NoSuchAlgorithmException;
 public class LostKey extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+         InetAddress addr = InetAddress.getLocalHost();
+
+        //Host IP Address
+        String ipAddress = addr.getHostAddress();
+        //Hostname
+        String hostname = addr.getHostName();
+        
         User uu = (User) request.getSession().getAttribute("auth");
+        DB.me().insert(new Log(Log.INFO,uu.getId(),ipAddress,"LOSTKEY",null,3));
+        
         KeyDao.getInstance().updateLostkey(uu.getId());
 
         response.sendRedirect("key.jsp");
